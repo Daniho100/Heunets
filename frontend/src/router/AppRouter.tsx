5d91eefc4eef71046d424cd6da1ecfedb046a965
Daniho100 <daniandre.dah@gmail.com>
2025-10-30 14:50:17 +0100

    Heunets deployment

--- frontend/src/router/AppRouter.tsx
+++ frontend/src/router/AppRouter.tsx
@@ -18,7 +18,7 @@ export default function AppRouter() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/sigup" element={<Signup />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
       
